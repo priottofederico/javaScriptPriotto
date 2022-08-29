e07a77a9a6d4c536f885f2b89af9a781991810c3
Federico Priotto <priottofederico@gmail.com>
2022-08-28 23:54:53 -0300

    correcciones

--- index.js
+++ index.js
@@ -1,22 +1,9 @@
 function Calculate() {
-  
-  // Extracting value in the amount 
-  // section in the variable
-  const amount = document.querySelector("#amount").value;
-
-  // Extracting value in the interest
-  // rate section in the variable
-  const rate = document.querySelector("#rate").value;
-
-  // Extracting value in the months 
-  // section in the variable
-  const months = document.querySelector("#months").value;
-
-  // Calculating interest per month
-  const interest = (amount * (rate * 0.01)) / months;
-    
-  // Calculating total payment
-  const total = ((amount / months) + interest).toFixed(2);
+  const monto = document.querySelector("#monto").value;
+  const interes = document.querySelector("#interes").value;
+  const mes = document.querySelector("#mes").value;
+  const interest = (monto * (interes * 0.01)) / mes;
+  const total = ((monto / mes) + interest).toFixed(2);
 
   document.querySelector("#total")
       .innerHTML = "Total de Cuota: " + total;
