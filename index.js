7868f9a9c926bdaab44880490faafc07ef2d682c
Federico Priotto <priottofederico@gmail.com>
2022-08-31 18:11:08 -0300

    complementario

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function Calculate() {
+/*function Calculate() {
   const monto = document.querySelector("#monto").value;
   const interes = document.querySelector("#interes").value;
   const mes = document.querySelector("#mes").value;
@@ -7,7 +7,26 @@ function Calculate() {
 
   document.querySelector("#total")
       .innerHTML = "Total de Cuota: " + total;
-}  
+}  */
+class amigo{
+  constructor(nombre, domicilio, edad, profesion){
+    this.nombre = nombre;
+    this.domicilio = domicilio;
+    this.edad = edad;
+    this.profesion = profesion;
+  }
+}
+const amigos = [];
+amigos.push(new amigo("Gabriel", "Primera Junta", 36, true));
+amigos.push(new amigo("Franco", "Gurruchaga", 30, false));
+amigos.push(new amigo("Bruno", "Solano", 40, true));
+amigos.push(new amigo("Lucas", "Rucci", 39, true));
+amigos.push(new amigo("Matias", "Ceres", 28, false));
+amigos.push(new amigo("Cristian", "Ceci", 18, false));
+amigos.push(new amigo("Ignacio", "Balbiano", 42, true));
+console.log(amigos);
+
+
 
 
 
